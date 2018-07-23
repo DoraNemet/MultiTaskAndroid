63e0f460f488f8e60d468739dfd9286a4e9b0991
Dora Fundak <dora.fundak@gmail.com>
2018-07-23 15:31:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dorafundak/multitask/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dorafundak.multitask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button fuelButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fuelButton = new Button(this);
+        fuelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BlueActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
