02be8a334a053a67040bcea4ce217eb0b733a163
Dora Fundak <dora.fundak@gmail.com>
2018-07-23 16:18:25 +0200

    Calculator and fuel layout

--- app/src/main/java/com/example/dorafundak/multitask/BlueActivity.java
+++ app/src/main/java/com/example/dorafundak/multitask/BlueActivity.java
@@ -1,13 +1,26 @@
 package com.example.dorafundak.multitask;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class BlueActivity extends AppCompatActivity {
 
+    Button calculatorButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue);
+
+        calculatorButton = (findViewById(R.id.calculatorButton));
+        calculatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BlueActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dorafundak/multitask/MainActivity.java
+++ app/src/main/java/com/example/dorafundak/multitask/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fuelButton = new Button(this);
+        fuelButton = (findViewById(R.id.fuelButton));
         fuelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
